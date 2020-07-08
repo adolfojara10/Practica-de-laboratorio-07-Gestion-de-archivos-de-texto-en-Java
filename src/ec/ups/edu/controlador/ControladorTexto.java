38cdfb9c2e9ae1023de16826e7bd2ccc6e76f247
Adolfo Jara <gavilanesadolfo@gmail.com>
2020-07-07 22:55:11 -0500

    En la clase controlador, los emtodos para generar el encriptado, crear un archivo, y para que el texto se encripte

--- src/ec/ups/edu/controlador/ControladorTexto.java
+++ src/ec/ups/edu/controlador/ControladorTexto.java
@@ -5,8 +5,12 @@
  */
 package ec.ups.edu.controlador;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.*;
 
 /**
  *
@@ -14,10 +18,42 @@ import java.io.IOException;
  */
 public class ControladorTexto {
 
-    String ruta;
-    File fichero;
+    private String ruta;
+    private File fichero;
+    private List<Character> abecedario;
+    private Map<Character, Character> diccionario;
 
     public ControladorTexto() {
+        abecedario = new ArrayList<>();
+        diccionario = new HashMap<>();
+        diccionario = crearDiccionario();
+    }
+
+    public Map<Character, Character> crearDiccionario() {
+        String abe = "abcdefghijklmnñopqrstuvwxyz";
+        String num = "0123456789";
+        String caritas = "☺☻♥♦♣♠•◘○◙♀";
+        String espacio = " ";
+        int aux = (abe.length() - 1);
+
+        for (int i = 0; i < abe.length(); i++) {
+            diccionario.put(abe.charAt(i), abe.charAt(aux));
+        }
+
+        diccionario.put(espacio.charAt(0), caritas.charAt(10));
+
+        diccionario.put(num.charAt(0), caritas.charAt(0));
+        diccionario.put(num.charAt(1), caritas.charAt(1));
+        diccionario.put(num.charAt(2), caritas.charAt(2));
+        diccionario.put(num.charAt(3), caritas.charAt(3));
+        diccionario.put(num.charAt(4), caritas.charAt(4));
+        diccionario.put(num.charAt(5), caritas.charAt(5));
+        diccionario.put(num.charAt(6), caritas.charAt(6));
+        diccionario.put(num.charAt(7), caritas.charAt(7));
+        diccionario.put(num.charAt(8), caritas.charAt(8));
+        diccionario.put(num.charAt(9), caritas.charAt(9));
+
+        return diccionario;
     }
 
     public boolean comprobar(String ruta, String nombre) {
@@ -35,16 +71,49 @@ public class ControladorTexto {
 
         nombre = nombre.concat(".txt");
         fichero = new File(ruta + File.separator + nombre);
-        String devolver = null;
 
         try {
-
             fichero.createNewFile();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
 
         return fichero.getAbsolutePath();
+    }
+
+    public void encriptar(String rutaAbsoluta, String texto) {
+        //fichero = new File(rutaAbsoluta);
+        texto = texto.toLowerCase();
+        String aux = null;
+
+        for (int i = 0; i < texto.length(); i++) {
+            char letra = texto.charAt(i);
+
+            for (Map.Entry<Character, Character> letra2 : diccionario.entrySet()) {
+
+                if (letra == letra2.getKey()) {
+                    aux = letra2.getValue().toString();
+                }
+            }
+        }
+
+        try {
+            FileWriter archivoEscritura = new FileWriter(rutaAbsoluta, true);
+
+            BufferedWriter escritura = new BufferedWriter(archivoEscritura);
+
+            escritura.append(aux);
+
+            escritura.close();
+            archivoEscritura.close();
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("Ruta del archivo no encontrada");
+        } catch (IOException ex2) {
+            System.out.println("Error de escritura");
+        } catch (Exception ex3) {
+            System.out.println("Error General");
+        }
     }
 
 }
